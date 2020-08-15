f386a6e4ac28c774fa359d49b9705017276c3761
anast.pro <anastpro@anast.pro>
2020-08-15 22:16:32 +0400

    Sprint4.Mesto v2

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,32 +2,34 @@ let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let popupCloseButton = popup.querySelector('.popup__close');
 let popupSubmitButton = popup.querySelector('.popup__submit');
-let name = document.querySelector('.profile__name'); //jnc.lf gkfu
+let name = document.querySelector('.profile__name');
 let aboutme = document.querySelector('.profile__aboutme');
-let nameInput = popup.querySelector('.popup__name');
-let aboutmeInput = popup.querySelector('.popup__aboutme');
+let nameInput = popup.querySelector('.popup__form-information_name');
+let aboutmeInput = popup.querySelector('.popup__form-information_aboutme');
+let formElement = popup.querySelector('.popup__form');
 
-let popupToggle = function() {
-    popup.classList.toggle('popup_opened');
+/*let popupToggle = function() {
+    popup.classList.toggle('popup_opened');}*/
+
+
+let openPopup = function() {
+    popup.classList.add('popup_opened');
 }
 
 let closePopup = function(event) {
     if (event.target !== event.currentTarget) return
-    popupToggle();
+    popup.classList.remove('popup_opened');
 }
-
-
-let formElement = popup.querySelector('.popup__form');
 
 function formSubmitHandler(event) {
     event.preventDefault();
     name.textContent = nameInput.value;
     aboutme.textContent = aboutmeInput.value;
-    popupToggle();
+    popup.classList.remove('popup_opened');
 
 }
 
-editButton.addEventListener('click', popupToggle);
-popupCloseButton.addEventListener('click', popupToggle);
+editButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
 popup.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
